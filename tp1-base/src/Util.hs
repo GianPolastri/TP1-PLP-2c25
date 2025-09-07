3056c46d0c32a1163a414cf793c20f27397a5bdc
goyo79 <gregoriolopezporto@gmail.com>
2025-09-07 17:01:50 -0300

    versión de Goyo Util.hs

--- tp1-base/src/Util.hs
+++ tp1-base/src/Util.hs
@@ -3,34 +3,18 @@ module Util where
 -- | @alinearDerecha n s@ agrega espacios a la izquierda de @s@ hasta que su longitud sea @n@.
 -- Si @s@ ya tiene longitud @>= n@, devuelve @s@.
 alinearDerecha :: Int -> String -> String
-alinearDerecha n s = if length s == n then s else e ++ s
-    where e = replicate (n - length s) ' '
-
-
-
--- alinearDerecha n s = if length s >= n then s else foldr (\p rec -> (++) (" " ++ p) rec) "" s;
-
+alinearDerecha n s  | length s >= n = s
+                    | otherwise     = replicate (n - length s) ' ' ++ s
 -- | Dado un índice y una función, actualiza el elemento en la posición del índice
 -- aplicando la función al valor actual. Si el índice está fuera de los límites
 -- de la lista, devuelve la lista sin cambios.
 -- El primer elemento de la lista es el índice 0.
 actualizarElem :: Int -> (a -> a) -> [a] -> [a]
-actualizarElem n f xs = actualizarElem' f xs n
+actualizarElem n f xs = actualizarElem2 f xs n
+actualizarElem2 :: (a -> a) -> [a] -> (Int -> [a])
+actualizarElem2 f = foldr (\x r -> (\n -> if (n==0) then f x : r (n-1) else x   : r (n-1))) (const [])
 
-actualizarElem' ::  (a -> a) -> [a] -> (Int -> [a])
-actualizarElem' f = foldr (\x rec -> \n -> if n == 0 then f x :rec (-1) else x: rec (n-1)) (const [])
-    
-            {- 
-                    
-                la lambda es una funcion que devuelve otra lambda (:: Int -> a).
 
-                Como la lambda devuelve una funcion de Int ->[a], a la recursion le tengo que pasar un Int 
-                para que pueda continuar con su recursion. Como estamos haciendo n-1 es como recorrer la lista
-                de der a izq (n denota el indice en la lista -posicion de x en la lista-). En la rama true pasamos
-                un (-1) para no afectar a n.
-                    
-            -}
-            
 -- | infinito positivo (Haskell no tiene literal para +infinito)
 infinitoPositivo :: Float
 infinitoPositivo = 1 / 0
